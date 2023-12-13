43fe0a2f2aef88f1d180218b37ef7127234e93ab
Fernanda Menezes <fernanda.plessim@mail.uft.edu.br>
2023-12-13 14:49:27 -0300

    Projeto de Estudo DjagoRest2

---
+++ clientes/validators.py
@@ -0,0 +1,16 @@
+import regex as re
+from validate_docbr import CPF
+
+def cpf_valido(numero_do_cpf):
+    cpf = CPF()
+    return  cpf.validate(numero_do_cpf)
+
+def nome_valido(nome):
+    return nome.isalpha()
+
+def rg_valido(rg):
+    return len(rg)== 9 
+
+def celular_valido(numero_celular):
+    modelo = '[0-9]{2} [0-9]{5}-[0-9]{4}'
+    return re.findall(modelo,numero_celular)
